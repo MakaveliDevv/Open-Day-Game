7f65409a09d9aab1a28a6977366d124683b259f5
eternitarian <lorenamardar@yahoo.com>
2024-03-04 14:24:48 +0100

    hguvyuvyibhbgyuvib

--- Context-Game/Assets/Scripts/PlayerMovement.cs
+++ Context-Game/Assets/Scripts/PlayerMovement.cs
@@ -40,7 +40,7 @@ public class PlayerMovement : MonoBehaviour
 
     void Update() 
     {
-        Debug.Log("Hi I'm lowRain");
+        Debug.Log("Hi I'm lowRain aaaaaaaaaaaaaaaaaaaaaaaaaaaaah");
         inputDirection = new(Input.GetAxisRaw("Horizontal"), 0f);
         rb.constraints = RigidbodyConstraints2D.FreezeRotation;
         Jump();
